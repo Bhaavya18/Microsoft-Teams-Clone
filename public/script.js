c41f6c450af204f3329bbb5927f1d76f04181068
Bhaavya Rampal <invinciblebhaavya@gmail.com>
2021-06-27 19:32:31 +0530

    first commit

---
+++ public/script.js
@@ -0,0 +1,51 @@
+const socket = io('/');
+const videoDisplay = document.getElementById('container');
+const myVideo = document.createElement('video');
+const peer = new Peer();
+peer.on('open', function(userid) { //connection established
+  socket.emit('joined-room', ROOM_ID, userid);
+  console.log("my id "+userid);
+});
+navigator.mediaDevices.getUserMedia({
+  video: true,
+  audio: true
+}).then(stream => {
+  addVideo(myVideo, stream);
+  peer.on('call', (call) => {
+    call.answer(stream);
+    console.log("hello");
+    const video = document.createElement('video')
+    call.on('stream', (userVideoStream) => {
+      addVideo(video, userVideoStream)
+    })
+  })
+  socket.on('user-connected', userId => {
+  console.log('New User Connected: ' + userId)
+  const fc = () => connectToNewUser(userId, stream)
+  timerid = setTimeout(fc, 2500)
+  })
+});
+const connectToNewUser = (userid, stream) => {
+  console.log("new user joined in "+userid);
+  const call = peer.call(userid, stream);
+  const video=document.createElement('video');
+  call.on('stream', (remoteStream) => {
+  //  console.log(remoteStream);
+    addVideo(video,remoteStream);
+  });
+}
+const addVideo = (video, stream) => {
+  video.srcObject = stream; //video to be streamed
+  video.addEventListener("onloadedmetadata", function() { //when metadata(duration,dimensions) of video is loaded then, play video
+    video.play();
+  });
+  video.autoplay=true;
+  video.muted=true;
+  videoDisplay.append(video);
+};
+timer();
+function timer(){
+var date=new Date();
+document.getElementById("time").innerHTML=date.getHours()+":"+date.getMinutes();
+setTimeout(timer,1000);
+}
---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app=express();
+const server=require('http').Server(app);//helps in creating a server to be used with socket.io
+const io=require('socket.io')(server);//for socket.io to know which server we are using
+const { v4: uuidv4 } = require('uuid');//version 4 of uuid used
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, { //peer server handles webRTC signalling for us
+  debug:true
+});
+app.use('/peerjs', peerServer);
+app.set('view engine','ejs');
+app.use(express.static("public"));
+app.get('/',function(req,res){
+  const rid=uuidv4();//unique room id
+  res.redirect(`/${rid}`);
+  //res.render('room',{roomid:rid});
+});
+app.get('/:roomid',function(req,res){
+  res.render('room',{roomid:req.params.roomid});
+});
+io.on('connection',function(socket){//handshake pipeline for bidirectional communication b/w client and server
+  socket.on('joined-room',function(roomid,userid){//listen to message sent by client ROOM_ID info
+  //  console.log(roomid);
+    socket.join(roomid);
+    socket.broadcast.to(roomid).emit('user-connected',userid);//braodcast will emit message to all the clients except to the new user
+  })
+  //socket.emit('joined-room',"okay now what's next?");
+});
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+server.listen(port,function(){
+  console.log("server is running on port 3000");
+});
